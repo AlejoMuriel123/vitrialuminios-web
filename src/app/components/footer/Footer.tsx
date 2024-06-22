89b47e22ec7924e5b3bf97ecc6998e07ae6d0dab
Aprendizvp2 <106331402+Aprendizvp2@users.noreply.github.com>
2024-06-22 09:39:18 -0500

    updated menu drawer

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -2,10 +2,9 @@ import { FacebookIcon, InstagramIcon, TwitterIcon } from "../../assets/svg";
 
 export default function Footer() {
   return (
-    <div className="px-10 xl:px-40 py-16 border border-t-gray-300 flex flex-col md:flex-row justify-between gap-8 items-center">
+    <div className="px-10 xl:px-40 py-16 border-t-[1px] border-t-gray-300 flex flex-col md:flex-row justify-between gap-8 items-center">
       <p className="text-gray-500 text-sm">
-        © Vitrialuminos del Valle {new Date().getFullYear()}, All Rights
-        Reserved.
+        © Vitrialuminos del Valle {new Date().getFullYear()}, All Rights Reserved.
       </p>
       <div className="flex flex-row gap-4">
         <a
