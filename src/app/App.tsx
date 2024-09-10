240b6bec86f5dea5f93556f52f575e57382e5047
Aprendizvp2 <106331402+Aprendizvp2@users.noreply.github.com>
2024-09-10 14:24:24 -0500

    fixed type issue in app.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { About, Gallery } from "./sections";
 import {
   OutOfServiceModal,
@@ -6,12 +7,13 @@ import {
   SmallHeader,
   CarouselHome,
 } from "./components";
+import ScrollAnimation from "./components/scrollanimation/ScrollAnimation";
+// @ts-expect-error
 import { FloatingWhatsApp } from "@carlos8a/react-whatsapp-floating-button";
-import "./App.css";
 import { useMobile } from "./hooks/useMobile";
+import "./App.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import ScrollAnimation from "./components/scrollanimation/ScrollAnimation";
 
 export default function App() {
   const { isMobile } = useMobile();
